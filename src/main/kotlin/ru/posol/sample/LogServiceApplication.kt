e85655dd086f4f55f61077953c0fa59f245637cf
POSOL-ПК\posol <posol2008@gmail.com>
2018-03-20 19:37:06 +0500

    add dev property for sql and loging

--- src/main/kotlin/ru/posol/sample/LogServiceApplication.kt
+++ src/main/kotlin/ru/posol/sample/LogServiceApplication.kt
@@ -1,9 +1,11 @@
 package ru.posol.sample
 
+import org.hibernate.dialect.H2Dialect
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.context.annotation.Bean
 import ru.posol.sample.service.LogService
 import ru.posol.sample.service.dto.LogDto
@@ -14,6 +16,17 @@ class LogServiceApplication {
 
     val logger = LoggerFactory.getLogger("ru.posol.sample.LogServiceApplication")
 
+    @Bean
+    @ConditionalOnClass(H2Dialect::class)
+    fun init(service: LogService): CommandLineRunner {
+        return CommandLineRunner {
+
+            service.createNewLog(LogDto("1997-07-16T19:20:30+01:00", "INFO", "error"),"user")
+            service.createNewLog(LogDto("1989-03-27T19:20:30+01:00", "INFO", "error"),"user")
+            service.createNewLog(LogDto("2001-07-16T19:20:30+01:00", "INFO", "error"),"user")
+            service.createNewLog(LogDto("2018-01-16T19:20:30+01:00", "INFO", "error"),"admin")
+        }
+    }
 
 }
 
