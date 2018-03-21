a4e6b5f3a9d7f5d284555c1e0df90fab8d839f7e
POSOL-ПК\posol <posol2008@gmail.com>
2018-03-21 21:49:25 +0500

    add swagger

--- src/main/kotlin/ru/posol/sample/LogServiceApplication.kt
+++ src/main/kotlin/ru/posol/sample/LogServiceApplication.kt
@@ -1,5 +1,6 @@
 package ru.posol.sample
 
+import com.google.common.base.Predicates
 import org.hibernate.dialect.H2Dialect
 import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -9,12 +10,26 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.context.annotation.Bean
 import ru.posol.sample.service.LogService
 import ru.posol.sample.service.dto.LogDto
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 
 
 @SpringBootApplication
+@EnableSwagger2
 class LogServiceApplication {
 
     val logger = LoggerFactory.getLogger("ru.posol.sample.LogServiceApplication")
+
+    @Bean
+    open fun api() : Docket = Docket(DocumentationType.SWAGGER_2)
+            .select()
+            .apis(RequestHandlerSelectors.any())
+            //скрывает лишнюю инфу
+            .paths(Predicates.not(PathSelectors.regex("/error.*")))
+            .build()
 
     @Bean
     @ConditionalOnClass(H2Dialect::class)
